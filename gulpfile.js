b029255e18dfc878546d6e23423d4dc098e17544
sczyh30 <sczyh16@gmail.com>
2015-12-20 14:22:48 +0800

    config update

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify  = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat  = require('gulp-concat'),
+    clean = require('gulp-clean');
+
+gulp.task('html', function () {
+
+});
+
+gulp.task('images', function () {
+
+});
+
+gulp.task('css', function () {
+
+});
+
+gulp.task('js', function () {
+
+});
+
+gulp.task('default', function () {
+
+});
+
+gulp.task('watch', function () {
+
+});
